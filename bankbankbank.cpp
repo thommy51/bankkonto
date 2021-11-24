6323c7e915984eab7c7be3d6ef2a1cdaacb92e3a
Thommy <Tho.Breuer@outlook.com>
2021-11-24 21:21:35 +0100

    changed names for variables and functions

--- bankbankbank.cpp
+++ bankbankbank.cpp
@@ -6,55 +6,57 @@
 using namespace std;
 
 string pin;
-string checkpin;
-string kontonr;
-string guthaben;
+string validatePin;
+string kontoNr;
+string bankBalance;
 string recipient;
-string betrag;
+string amount;
 
 int kontoStart();
-int einzahlung(string& s);
-int auszahlen(string& s);
-int checkguthaben(void* NotUsed, int argc, char** argv, char** azColName);
-int guthabenabfrage();
-int transfer(string& r, string& b);
+int depositMoney(string& s);
+int withdrawMoney(string& s);
+int getBalance(void* NotUsed, int argc, char** argv, char** azColName);
+int balanceQuery();
+int transferMoney(string& r, string& b);
+int checkKontoNr(string& s);
+int getPin(void* NotUsed, int argc, char** argv, char** azColName);
+int checkPin(string& s);
 
 
 
+int main()
+{
+    cout << "Geben Sie ihre Kontonr ein: "; cin >> kontoNr;
+    checkKontoNr(kontoNr);
 
-int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    cout << "Geben Sie ihre Pin ein: "; cin >> pin;
+    checkPin(pin);
+
+    return 0;
+}
+int getPin(void* NotUsed, int argc, char** argv, char** azColName) {
     int i;
     for (i = 0; i < argc; i++) {
-        checkpin = argv[i];
+        validatePin = argv[i];
     }
     return 0;
 }
-
-
-int startQuery(string &s) {
+int checkKontoNr(string &s) {
     sqlite3* db;
     char* zErrMsg = 0;
     string rc;
 
-
     rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
     string query = "SELECT pin from user WHERE kontonr = " + s;
-    rc = sqlite3_exec(db, query.c_str(), callback, NULL, &zErrMsg);
+    rc = sqlite3_exec(db, query.c_str(), getPin, NULL, &zErrMsg);
 
     sqlite3_close(db);
 
     return 0;
 }
-int checkguthaben(void* NotUsed, int argc, char** argv, char** azColName) {
-    int i;
-    for (i = 0; i < argc; i++) {
-        guthaben = argv[i];
-        return 0;
-    }
-}
-int checkPin(string &s) {
+int checkPin(string& s) {
 
-    if (checkpin == pin)
+    if (validatePin == pin)
     {
         system("cls");
         kontoStart();
@@ -65,92 +67,82 @@ int checkPin(string &s) {
 
     return 0;
 }
-
 int kontoStart() {
 
-    int entscheidung;
-    string einzahlen;
-    string ausbezahlen;
+    int choice;
+    string putMoney;
+    string takeMoney;
 
-    do{
+    do {
         system("cls");
-        guthabenabfrage();
-        std::cout << "\nIhr Kontostand betraegt: " << guthaben;
+        balanceQuery();
+        std::cout << "\nIhr Kontostand betraegt: " << bankBalance;
 
         cout << "\n\n 1. Moechten Sie Geld einzahlen ?";
         cout << "\n 2. Moechten Sie Geld auszahlen ?";
-        cout << "\n 3. Geld zu überweisen.\n\n";
+        cout << "\n 3. Geld zu ueberweisen.\n\n";
         cout << "\n 4. Um zu verlassen.\n\n";
-        cin >> entscheidung;
+        cin >> choice;
 
-        if (entscheidung == 1)
+        if (choice == 1)
         {
             system("cls");
 
-            cout << "Wie viel Geld moechten Sie einzahlen ?: "; cin >> einzahlen;
+            cout << "Wie viel Geld moechten Sie einzahlen ?: "; cin >> putMoney;
 
-            einzahlung(einzahlen);
+            depositMoney(putMoney);
 
         }
-        else if (entscheidung == 2) {
+        else if (choice == 2) {
             system("cls");
 
-            cout << "Wie viel Geld moechten Sie ausbezahlen ?: "; cin >> ausbezahlen;
+            cout << "Wie viel Geld moechten Sie ausbezahlen ?: "; cin >> takeMoney;
 
-            auszahlen(ausbezahlen);
+            withdrawMoney(takeMoney);
         }
-        else if (entscheidung == 3) {
+        else if (choice == 3) {
             system("cls");
 
             cout << ("Bitte geben Sie die Kontonr des Empfaengers ein: "); cin >> recipient;
-            cout << ("Bitte geben Sie den Betrag ein: "); cin >> betrag;
+            cout << ("Bitte geben Sie den Betrag ein: "); cin >> amount;
 
-            transfer(recipient, betrag);
-           
+            transferMoney(recipient, amount);
+
         }
-    } while (entscheidung != 4);
+    } while (choice != 4);
     return 0;
 }
-
-int transfer(string& r, string& b) {
-    sqlite3* db;
-    string rc;
-
-    rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
-
-    string query = "UPDATE user SET guthaben = (guthaben - " + b + ") WHERE kontonr = " + kontonr + "; UPDATE user SET guthaben = (guthaben + " + b + ") WHERE kontonr = " + r +";";
-
-    rc = sqlite3_exec(db, query.c_str(), checkguthaben, NULL, NULL);
-
-    sqlite3_close(db);
-
-    return 0;
-}
-
-int guthabenabfrage() {
+int balanceQuery() {
 
     sqlite3* db;
     string rc;
 
     rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
 
-    string query = "SELECT guthaben FROM user WHERE kontonr =" + kontonr;
+    string query = "SELECT guthaben FROM user WHERE kontonr =" + kontoNr;
 
-    rc = sqlite3_exec(db, query.c_str(), checkguthaben, NULL, NULL);
+    rc = sqlite3_exec(db, query.c_str(), getBalance, NULL, NULL);
 
     sqlite3_close(db);
 
     return 0;
 
 }
+int getBalance(void* NotUsed, int argc, char** argv, char** azColName) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        bankBalance = argv[i];
+        return 0;
+    }
+}
 
-int einzahlung(string &s) {
+int depositMoney(string &s) {
     sqlite3* db;
     string rc;
 
     rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
 
-    string query = "UPDATE user SET guthaben = ( guthaben +"+ s +") WHERE kontonr = " + kontonr;
+    string query = "UPDATE user SET guthaben = ( guthaben +"+ s +") WHERE kontonr = " + kontoNr;
 
     rc = sqlite3_exec(db, query.c_str(), NULL, NULL, NULL);
 
@@ -158,14 +150,13 @@ int einzahlung(string &s) {
 
     return 0;
 }
-
-int auszahlen(string& s) {
+int withdrawMoney(string& s) {
     sqlite3* db;
     string rc;
 
     rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
 
-    string query = "UPDATE user SET guthaben = ( guthaben -" + s + ") WHERE kontonr = " + kontonr;
+    string query = "UPDATE user SET guthaben = ( guthaben -" + s + ") WHERE kontonr = " + kontoNr;
 
     rc = sqlite3_exec(db, query.c_str(), NULL, NULL, NULL);
 
@@ -173,15 +164,17 @@ int auszahlen(string& s) {
 
     return 0;
 }
+int transferMoney(string& r, string& b) {
+    sqlite3* db;
+    string rc;
 
-int main()
-{
+    rc = sqlite3_open("E:\\sqlite\\databases\\bank.db", &db);
 
-    cout << "Geben Sie ihre Kontonr ein: "; cin >> kontonr;
-    startQuery(kontonr);
+    string query = "UPDATE user SET guthaben = (guthaben - " + b + ") WHERE kontonr = " + kontoNr + "; UPDATE user SET guthaben = (guthaben + " + b + ") WHERE kontonr = " + r + ";";
 
-    cout << "Geben Sie ihre Pin ein: "; cin >> pin;
-    checkPin(pin);
+    rc = sqlite3_exec(db, query.c_str(), getBalance, NULL, NULL);
+
+    sqlite3_close(db);
 
     return 0;
 }
